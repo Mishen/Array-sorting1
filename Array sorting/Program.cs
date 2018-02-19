44f9b7c4f018d084b55452a729a78c6ff2df0825
Mishen <mishen888@yandex.ru>
2018-02-19 20:09:23 +0300

    1st

--- Array sorting/Program.cs
+++ Array sorting/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
-namespace Array_sorting
+namespace SortApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // input
+            int[] nums = new int[7];
+            Console.WriteLine("Insert twenty numbers");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0} number: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            // sorting
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            // output
+            Console.WriteLine("Sorting array output");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
